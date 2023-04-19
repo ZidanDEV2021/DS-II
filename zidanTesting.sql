364dab6266d28ecc6fd8c2575c9ec9f0105117ef
Thomas <44611711+Greekler@users.noreply.github.com>
2023-04-19 11:08:27 +0200

    Update zidanTesting.sql

--- zidanTesting.sql
+++ zidanTesting.sql
@@ -11,17 +11,17 @@ BEGIN
   
   IF NOT EXISTS(SELECT * FROM games WHERE id = :NEW.game_id) THEN
     dbms_output.put_line('The game does not exist');
-    RAISE_APPLICATION_ERROR(-20001, 'The game does not exist');
+    RETURN;
   ELSIF :NEW.referee_type NOT IN ('referee', 'linesman') THEN
     dbms_output.put_line('Referee type does not exist');
-    RAISE_APPLICATION_ERROR(-20002, 'Referee type does not exist');
+    RETURN;
   ELSIF num_referees >= 4 THEN
     dbms_output.put_line('Maximum number of referees per game');
-    RAISE_APPLICATION_ERROR(-20003, 'Maximum number of referees per game');
+    RETURN;
   ELSE
     -- All checks pass, assign the referee to the game
     INSERT INTO games_referees (game_id, referee_name, referee_type) VALUES (:NEW.game_id, :NEW.name, :NEW.referee_type);
   END IF;
 END;
 
-RAISE_APPLICATION odstraň to asi ani neexistuje, moc nevím jak ukončit ten TRIGGER po tom output line ale zkusím na to přijít, zatím podle mě ok.
+RAISE_APPLICATION jsem změnil na RETURN ale nejsem si jisty jestli to je spravne
