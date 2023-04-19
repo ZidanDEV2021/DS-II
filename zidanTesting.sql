24f5ec5744f2aaec59fcfad7b7238e7adc640cf5
Thomas <44611711+Greekler@users.noreply.github.com>
2023-04-19 19:57:11 +0200

    Update zidanTesting.sql

--- zidanTesting.sql
+++ zidanTesting.sql
@@ -31,3 +31,36 @@ BEGIN
         WHERE GAME_ID = :OLD.GAME_ID AND TEAM_ID = :OLD.TEAM_ID_FOR;
     END IF;
 END;
+
+-------
+
+CREATE OR REPLACE PROCEDURE PCheckGame(p_gameID GAME_OFFICIALS.GAME_ID%TYPE, p_gameName GAME_OFFICIALS.OFFICIAL_NAME%TYPE, p_offType GAME_OFFICIALS.OFFICIAL_TYPE%TYPE) AS
+    v_gameID INT;
+    v_games_count INT;
+BEGIN
+    
+    SELECT GAME_ID INTO v_gameID FROM GAME_OFFICIALS WHERE GAME_ID = p_gameID;
+    
+    IF p_gameID != v_gameID THEN
+        dbms_output.put_line('Hra neexistuje');
+        RETURN;
+    END IF;
+    
+    IF p_offType != 'linesman' AND p_offType != 'referee' THEN
+        dbms_output.put_line('Neexistující typ rozhodčího');
+        RETURN;
+    END IF;
+    
+    SELECT COUNT(*) INTO v_games_count
+    FROM GAME JOIN GAME_OFFICIALS ON GAME.GAME_ID = GAME_OFFICIALS.GAME_ID
+    WHERE GAME_OFFICIALS.OFFICIAL_NAME LIKE p_gameName AND GAME.DATE_TIME_GMT BETWEEN '10-MAR-15' AND '26-SEP-16'; 
+    
+    IF v_games_count > 100 THEN
+        dbms_output.put_line('Neexistující typ rozhodčího');
+        RETURN;
+    ELSE
+        INSERT INTO GAME_OFFICIALS(GAME_ID, OFFICIAL_NAME, OFFICIAL_TYPE)
+        VALUES (p_gameID, p_gameName, p_offType);
+    END IF;
+
+END;
