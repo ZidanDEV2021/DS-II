28dfcb31bb6704b13e2f13c703582b04ed465ca0
maawtyn <sostokmartin@gmail.com>
2023-04-20 07:38:22 +0200

    Update sadge.sql

--- sadge.sql
+++ sadge.sql
@@ -63,3 +63,68 @@ We determine the new game ID by finding the highest existing ID in the Game tabl
 
 We insert a new record into the Game table with the new game ID,
 */
+
+
+-- 2.
+
+CREATE OR REPLACE FUNCTION F_Export_Game_Stats (
+    p_game_id IN Game.game_id%TYPE
+) RETURN CLOB AS
+    v_xml CLOB;
+BEGIN
+    SELECT XMLAGG(
+        XMLELEMENT("game",
+            XMLFOREST(home_team_id AS "home_team_id", home_team_name AS "home_team_name",
+                      away_team_id AS "away_team_id", away_team_name AS "away_team_name"),
+            XMLAGG(
+                XMLELEMENT("player",
+                    XMLFOREST(p.first_name AS "first_name", p.last_name AS "last_name", t.team_name AS "team_name"),
+                    XMLATTRIBUTES(p.player_id AS "player_id", s.goals AS "goals")
+                )
+            )
+        )
+    )
+    INTO v_xml
+    FROM (
+        SELECT g.home_team_id, t1.team_name AS home_team_name,
+               g.away_team_id, t2.team_name AS away_team_name
+        FROM Game g
+        JOIN Team t1 ON g.home_team_id = t1.team_id
+        JOIN Team t2 ON g.away_team_id = t2.team_id
+        WHERE g.game_id = p_game_id
+    ) game
+    JOIN (
+        SELECT s.player_id, s.team_id, s.goals, ROW_NUMBER() OVER (ORDER BY s.goals DESC) AS rank
+        FROM Score s
+        WHERE s.game_id = p_game_id
+    ) s ON game.home_team_id = s.team_id OR game.away_team_id = s.team_id
+    JOIN Player p ON s.player_id = p.player_id
+    JOIN Team t ON s.team_id = t.team_id
+    WHERE s.rank <= 3
+    GROUP BY game.home_team_id, game.home_team_name, game.away_team_id, game.away_team_name;
+    
+    RETURN v_xml;
+END F_Export_Game_Stats;
+
+/*
+Explanation of the code:
+
+We declare the function with one input parameter p_game_id of type Game.game_id%TYPE and return type CLOB.
+
+We declare a variable v_xml of type CLOB to store the XML string.
+
+We use the XMLAGG and XMLELEMENT functions to generate the XML string. We start by creating a root element named "game" and add child elements for the home and away teams, using the XMLFOREST function to specify the element names and values.
+
+We use a subquery to get the team names for the given game ID.
+
+We join the Score table to get the players who scored goals in the match, and use the ROW_NUMBER function to rank them by the number of goals.
+
+We join the Player and Team tables to get the names of the players and the team they played for, and filter the results to include only the top 3 scorers.
+
+We group the results by the home and away team IDs and names.
+
+Finally, we return the XML string using the RETURN statement.
+
+Note: This code assumes that the Game, Team, Score, and Player tables are already created in the database and contain the necessary columns. It also assumes that the Score table has a foreign key constraint on Game.game_id and Team.team_id.
+
+*/
