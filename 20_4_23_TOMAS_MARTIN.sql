f41a0530f75ea357c7c70fd524461d746c3f750b
DDZidanek <131339025+DDZidanek@users.noreply.github.com>
2023-04-20 10:19:23 +0200

    Update 20_4_23_TOMAS_MARTIN.sql

--- 20_4_23_TOMAS_MARTIN.sql
+++ 20_4_23_TOMAS_MARTIN.sql
@@ -3,13 +3,13 @@ CREATE OR REPLACE TRIGGER T_Update_Team_Stat
 AFTER UPDATE OF goals ON Game_Teams_Stats
 FOR EACH ROW
 BEGIN
-    IF :new.hoa like 'home' THEN
+    IF :new.hoa like 'home' AND :new.goals <> :old.goals THEN
         UPDATE Game
-        SET home_goals = (home_goals + (:new.goals - :old.goals))
+        SET home_goals = home_goals + (:new.goals - :old.goals)
         WHERE game_id = :new.game_id;
-    ELSIF :new.hoa like 'away' THEN
+    ELSIF :new.hoa like 'away' AND :new.goals <> :old.goals THEN
         UPDATE Game
-        SET away_goals = (away_goals + (:new.goals - :old.goals))
+        SET away_goals = away_goals + (:new.goals - :old.goals)
         WHERE game_id = :new.game_id;
     END IF;
 END; vymaz si manualne stary trigger neslo to lvuli toho, mel jsem porad ulozeny ten stary trigger a neslo to pres drop vis co
