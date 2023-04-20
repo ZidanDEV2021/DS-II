334dd4cb12a67df37f545661845fea3f3894952f
DDZidanek <131339025+DDZidanek@users.noreply.github.com>
2023-04-20 09:36:24 +0200

    Update 20_4_23_TOMAS_MARTIN.sql

--- 20_4_23_TOMAS_MARTIN.sql
+++ 20_4_23_TOMAS_MARTIN.sql
@@ -1,26 +1,21 @@
-CREATE TRIGGER T_Update_Team_Stats
-AFTER UPDATE ON Game_Team_Stats
+CREATE OR REPLACE TRIGGER T_Update_Team_Stat
+AFTER UPDATE OF goals ON Game_Teams_Stats
 FOR EACH ROW
+DECLARE
+    home_goals NUMBER;
+    away_goals NUMBER;
 BEGIN
-  DECLARE home_goals INT;
-  DECLARE away_goals INT;
-  
-  SELECT SUM(CASE WHEN team_id = NEW.home_team_id THEN goals ELSE 0 END) INTO home_goals
-  FROM Game_Team_Stats
-  WHERE game_id = NEW.game_id;
-  
-  SELECT SUM(CASE WHEN team_id = NEW.away_team_id THEN goals ELSE 0 END) INTO away_goals
-  FROM Game_Team_Stats
-  WHERE game_id = NEW.game_id;
-  
-  IF NEW.goals <> OLD.goals THEN
-    UPDATE Game SET
-      home_team_goals = home_team_goals + (CASE WHEN NEW.team_id = NEW.home_team_id THEN NEW.goals - OLD.goals ELSE 0 END),
-      away_team_goals = away_team_goals + (CASE WHEN NEW.team_id = NEW.away_team_id THEN NEW.goals - OLD.goals ELSE 0 END)
-    WHERE id = NEW.game_id;
-  END IF;
-END;
+    SELECT home_team_id, away_team_id INTO home_goals, away_goals
+    FROM Game join Game_Teams_Stats on game.game_id = Game_Teams_Stats.game_id
+    WHERE Game_Teams_Stats.game_id = :new.game_id;
 
-Nedava to moc smysl  je to 1B ?
-To SELECT SUM(CASE WHEN team_id = NEW.home_team_id THEN goals ELSE 0 END) INTO home_goals nejak nesedí 
-je to 1b chat gpt je mimo trosku
+    IF :new.team_id = home_goals THEN
+        UPDATE Game
+        SET home_goals = home_goals + (:new.goals - :old.goals)
+        WHERE game_id = :new.game_id;
+    ELSIF :new.team_id = away_goals THEN
+        UPDATE Game
+        SET away_goals = away_goals + (:new.goals - :old.goals)
+        WHERE game_id = :new.game_id;
+    END IF;
+END; tohle se spousti
