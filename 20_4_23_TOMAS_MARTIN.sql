c042e4f656a0bb909f2c8cbe4b7b67abaa1c6961
DDZidanek <131339025+DDZidanek@users.noreply.github.com>
2023-04-20 10:14:10 +0200

    Update 20_4_23_TOMAS_MARTIN.sql

--- 20_4_23_TOMAS_MARTIN.sql
+++ 20_4_23_TOMAS_MARTIN.sql
@@ -1,27 +1,15 @@
+
 CREATE OR REPLACE TRIGGER T_Update_Team_Stat
 AFTER UPDATE OF goals ON Game_Teams_Stats
 FOR EACH ROW
-DECLARE
-    home_goals NUMBER;
-    away_goals NUMBER;
 BEGIN
-    SELECT home_team_id, away_team_id INTO home_goals, away_goals
-    FROM Game join Game_Teams_Stats on game.game_id = Game_Teams_Stats.game_id
-    WHERE Game_Teams_Stats.game_id = :new.game_id;
-
-    IF :new.team_id = home_goals THEN
+    IF :new.hoa like 'home' THEN
         UPDATE Game
-        SET home_goals = home_goals + (:new.goals - :old.goals)
+        SET home_goals = (home_goals + (:new.goals - :old.goals))
         WHERE game_id = :new.game_id;
-    ELSIF :new.team_id = away_goals THEN
+    ELSIF :new.hoa like 'away' THEN
         UPDATE Game
-        SET away_goals = away_goals + (:new.goals - :old.goals)
+        SET away_goals = (away_goals + (:new.goals - :old.goals))
         WHERE game_id = :new.game_id;
     END IF;
-END; tohle se spousti
-
-ORA-04091: table NEU0093.GAME_TEAMS_STATS is mutating, trigger/function may not see it
-ORA-06512: at "NEU0093.T_UPDATE_TEAM_STATS", line 5
-ORA-04088: error during execution of trigger 'NEU0093.T_UPDATE_TEAM_STATS'
-
-kdyz executnu UPDATE
+END; vymaz si manualne stary trigger neslo to lvuli toho
